94e895abd792774bf7c04389a00c2d7264467db2
Alen Veselič <68716124+AlenVeselic@users.noreply.github.com>
2020-11-30 21:44:25 +0100

    Chapter 11 finished, 12 started
    The debug chapter is through and now we're onto the chapter I was most looking forward to 'Web Scraping'.

--- errorTesting.py
+++ errorTesting.py
@@ -1,17 +1,3 @@
-import pyinputplus as pyip 
-def addsUpToTen(numbers):
-    numbersList = list(numbers)
-    for i, digit in enumerate(numbersList):
-        numbersList[i] = int(digit)
-    if sum(numbersList) != 10:
-        raise Exception('The digits must add up to 10, not %s.' % (sum(numbersList)))
-    return int(numbers) # Return an int form of numbers
-
-
-response = pyip.inputCustom(addsUpToTen) # No parentheses after addsUpToTen here.
-
-print(response)
-
-response = pyip.inputCustom(addsUpToTen)
-
-print(response)
+eggs = 'cherries'
+bacon = 'CHerries'
+assert False
