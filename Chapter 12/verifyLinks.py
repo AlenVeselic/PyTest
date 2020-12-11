037f6ba39b89bfee2cdd9ddd0bbc3952d432233c
Alen Veselič <68716124+AlenVeselic@users.noreply.github.com>
2020-12-11 03:19:30 +0100

    Chapter 12 finished
    Link Verification is done in a miniscule form(for forum posts on a slovenian forum site.). And with that I end the web scraping chapter in Automate the boring stuff.
    
    Offcourse this does not close the chapter on web scraping in my future endeavours. For you see, this chapter, beside my curiosity for the language, was why I bought the book in the first place. I shall continue to brush my interest in web sraping after I have finished this book.
    
    Now onto Working with Excel Spreadsheets!

--- Chapter 12/verifyLinks.py
+++ Chapter 12/verifyLinks.py
@@ -1,8 +1,13 @@
-import requests, bs4
+import requests, bs4, re, logging
 
-someUrl = ""
+logging.basicConfig(level= logging.DEBUG, format= ' %(asctime)s - %(levelname)s - %(message)s')
 
-noneExistentUrls = []
+someUrl = "https://slo-tech.com/forum/t2467#crta"
+
+linkRegex = re.compile(r'^(http|https)://\w+')
+
+
+nonExistentUrls = []
 response = requests.get(someUrl)
 if response.status_code == 404:
     print("Oh, so you gave me a broken link outright, eh? How could you?!")
@@ -10,11 +15,19 @@ else:
 
     resSoup = bs4.BeautifulSoup(response.text, 'html.parser')
 
-    links = resSoup.select('a')
-
+    links = resSoup.select('#content a')
+    print('There are %s links on this page.' % len(links))
     for link in range(len(links)):
-        linkCheck = requests.get(links[link].get('href'))
+        if linkRegex.search(links[link].get('href')):
+            logging.debug("Regex true, url: " + str(links[link].get('href')))
+            linkCheck = requests.get(links[link].get('href'))
+        else:
+            logging.debug("Regex false, url: " + str(links[link].get('href')))
+            linkCheck = requests.get("https://slo-tech.com" + links[link].get('href'))
+            
         if linkCheck.status_code == 404:
-            noneExistentUrls.append(linkCheck.url)
+            nonExistentUrls.append(linkCheck.url)
             print("Broken link found!")
 
+print('Done.')
+print('Broken links: ' + str(nonExistentUrls))
